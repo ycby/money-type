8bf27fa5843fcd1b05898fe418faf7d3d3fead33
ycby <bryan_yc@yahoo.com>
2026-02-27 18:10:27 +0800

    changed to use static method for init

--- src/Money.ts
+++ src/Money.ts
@@ -3,27 +3,57 @@ export default class Money {
     whole: number | bigint;
     fractional: number;
     decimal_places: number;
-    iso_code: string;
+    iso_code: string | undefined;
 
-    constructor(value: number | bigint, decimal_places: number, iso_code: string) {
-
-        if (value === undefined || value === null) throw new Error('Value must not be undefined or null');
-
-        if (typeof value === 'bigint') {
-
-            this.whole = value / (10n ** BigInt(decimal_places));
-            this.fractional = Number(value % (10n ** BigInt(decimal_places)));
-        } else {
-
-            this.whole = Math.trunc(value * (10 ** -decimal_places));
-            this.fractional = value % (10 ** decimal_places);
-        }
-
+    constructor(whole: number | bigint, fractional: number, decimal_places: number = 0, iso_code?: string) {
+        this.whole = whole;
+        this.fractional = fractional;
         this.decimal_places = decimal_places;
         this.iso_code = iso_code;
     }
 
-    getValue(): number {
+    static fromSmallestDenomination(value: number | bigint, decimal_places: number, iso_code: string) {
+
+        if (value === undefined || value === null) throw new Error('Value must not be undefined or null');
+
+        let whole: number | bigint;
+        let fractional: number;
+
+        if (typeof value === 'bigint') {
+
+            whole = value / (10n ** BigInt(decimal_places));
+            fractional = Number(value % (10n ** BigInt(decimal_places)));
+        } else {
+
+            whole = Math.trunc(value * (10 ** -decimal_places));
+            fractional = value % (10 ** decimal_places);
+        }
+
+        return new Money(whole, fractional, decimal_places, iso_code);
+    }
+
+    static fromNominalValue(value: number | bigint, decimal_places: number, iso_code: string) {
+
+        if (value === undefined || value === null) throw new Error('Value must not be undefined or null');
+
+        let whole: number | bigint;
+        let fractional: number;
+
+        if (typeof value === 'bigint') {
+
+            //fractional is 0 because a bigint cannot have fractional
+            whole = value;
+            fractional = 0;
+        } else {
+
+            whole = Math.trunc(value);
+            fractional = value % 1;
+        }
+
+        return new Money(whole, fractional, decimal_places, iso_code);
+    }
+
+    getNominalValue(): number {
 
         return Number(`${this.whole}.${this.fractional}`);
     }
